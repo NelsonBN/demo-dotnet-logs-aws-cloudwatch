eb372c1eadf9383b92beb5c6407366145c309d7f
Nelson Nobre <technobre@gmail.com>
2022-04-02 04:38:38 +0100

    Kickoff

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # demo-dotnet-logs-aws-cloudwatch
 Demo .NET for logs in AWS CloudWatch
+
+
+
+## Dependencies
+- AWS.Logger.AspNetCore
+
+
+
+## Run project
+
+
+### Build docker
+```bash
+docker build -f ./Dockerfile --force-rm -t technobre/demo-dotnet-logs-aws-cloudwatch .
+```
+
+
+### Run docker
+```bash
+docker rm demo-dotnet-logs-aws-cloudwatch -f
+docker run -d --name=demo-dotnet-logs-aws-cloudwatch -p 801:80 -t technobre/demo-dotnet-logs-aws-cloudwatch .
+```
+
---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddAWSProvider();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
